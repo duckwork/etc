2fa7d136201bad92e8da6c38181cd2e58c654411
Case Duckworth <acdw@acdw.net>
2021-08-24 23:19:55 -0500

    Search multiple files for __git_ps1 function

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -7,17 +7,29 @@ PROMPT_DIRTRIM=3 # how many dirs above current to print (rest are '...')
 PS1+='\[\e[36m\]\u@\h \w'
 
 # git bit
-source /usr/share/git/git-prompt.sh &&
-    PS1+='\[\e[35m\]$(__git_ps1)'
+# see https://unix.stackexchange.com/questions/278206
+possible_git_prompt_locations=(
+		/usr/share/git/git-prompt.sh # Arch, etc. (default?)
+		/usr/lib/git-core/git-sh-prompt # Debian, Ubuntu ...
+		/usr/share/git-core/contrib/completion/git-prompt.sh # Fedora ??
+)
+
+for file in "${possible_git_prompt_locations[@]}"; do
+		if [[ -f "$file" ]]; then
+				source "$file" &&
+						PS1+='\[\e[35m\]$(__git_ps1)'
+				break
+		fi
+done
 
 # newline
 PS1+='\[\e[0m\]\n'
 
 # exit code (only if error)
 __prompt_exit_code() {
-    local ec=$?
-    (( $ec > 0 )) &&
-        printf "$ec"
+		local ec=$?
+		(( $ec > 0 )) &&
+				printf "$ec"
 }
 PS1+='\[\e[31m\]$(__prompt_exit_code)\[\e[0m\]'
 
