2a86fffb617607ed9e302113563761a6d3cbc09f
Case Duckworth <acdw@acdw.net>
2021-09-05 12:07:21 -0500

    Change some variables

--- profile/etc.sh
+++ profile/etc.sh
@@ -2,6 +2,10 @@
 
 export GUILE_INSTALL_LOCALE=0
 export TZ=America/Chicago
+export QT_STYLE_OVERRIDE=adwaita-qt
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/keyring/ssh"
+
+export CDPATH=:~ # thanks june :)
 
 #export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
 #export XSERVERRC="$XDG_CONFIG_HOME/X11/xserverrc"
