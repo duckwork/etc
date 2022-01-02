609acec16e6a7867b4bcf507096b767fab8a14dd
Case Duckworth <acdw@acdw.net>
2022-01-02 10:01:49 -0600

    Use keychain(1) if available
    I need to add this to an install list

--- profile/ssh-agent.sh
+++ profile/ssh-agent.sh
@@ -1,3 +1,10 @@
 # start the ssh-agent
 
-eval $(ssh-agent -s)
+# use keychain(1), if available
+if type keychain > /dev/null 2>&1; then
+    # Save directory name in a variable (for ease of maintenance)
+    export KEYCHAIN_HOME="$HOME/.keychain"
+    eval $(keychain --eval --dir "$KEYCHAIN_HOME" --agents ssh)
+else
+    eval $(ssh-agent -s)
+fi
