8b352cf25190e877a00343153fd74a3c59d93066
Case Duckworth <acdw@acdw.net>
2022-03-13 05:31:16 +0000

    Move please to functions.bash

--- bash/functions.bash
+++ bash/functions.bash
@@ -1,3 +1,5 @@
+# Functions
+
 memq() { # memq ITEM ARRAY
     # Test whether an ITEM is a member of ARRAY.
     # Pass ARRAY as ${ARRAY[@]}.
@@ -8,4 +10,17 @@ memq() { # memq ITEM ARRAY
         }
     done
     return 1
+}
+
+please() { # please [COMMAND...]
+	# if run without arguments, run the last command with 'sudo' (aka sudo !!)
+	# if run WITH arguments, alias as sudo
+	history -d -1
+	if [ -z "$1" ]; then
+		#set --	$(HISTTIMEFORMAT=$'\t' history 2 | sed 's/^.*\t//;q')
+		set -- $(fc -lnr | sed 1q)
+	fi
+	echo >&2 sudo "$@"
+	history -s sudo "$@"
+	"${DEBUG:-false}" || sudo "$@"
 }
