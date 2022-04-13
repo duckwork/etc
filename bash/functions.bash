bc2772d47015d69ddb35c3cfb034aae66dabdd7a
Case Duckworth <acdw@acdw.net>
2022-04-13 17:08:29 -0500

    Add functions

--- bash/functions.bash
+++ bash/functions.bash
@@ -9,3 +9,22 @@ memq() { # memq ITEM ARRAY
     done
     return 1
 }
+
+rebashrc() { # rebashrc
+    ## Reload ~/.bashrc
+    printf "Loading ~/.bashrc..." >&2
+    if source "$HOME/.bashrc"; then
+        echo "OK." >&2
+    else
+        echo "ERROR!" >&2
+    fi
+}
+
+first_which() { # first_which COMMAND...
+    ## Return the fully-qualified path of the first COMMAND found in $PATH.
+    while :; do
+        command -v "$1" && break
+        [ -z "$1" ] && return 1
+        shift
+    done
+}
